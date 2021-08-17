892ded0d5008cdd24e09f58d1b2a2c09e04d455d
dsobolev <denis.sobolev1@gmail.com>
2021-08-17 17:44:53 +0300

    Turned Tweet Box to a component

---
+++ resources/views/components/tweet-box.blade.php
@@ -0,0 +1,21 @@
+<section class="border border-blue-400 rounded-lg mt-10 lg:mr-6 p-6">
+    <form action="">
+
+        <textarea
+            class="w-full"
+            placeholder="I'd like to say..."
+        ></textarea>
+
+        <hr class="my-4">
+
+        <footer class="flex justify-between">
+            <img
+                src="https://i.pravatar.cc/40"
+                alt="avatar"
+                class="rounded-full mr-2"
+            />
+            <x-button class="bg-blue-400 shadow">Send</x-button>
+        </footer>
+
+    </form>
+</section>
