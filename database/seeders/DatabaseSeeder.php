a73a0acf2a50bd4893f3809e9fd9c5550e37f191
dsobolev <denis.sobolev1@gmail.com>
2021-08-18 14:39:08 +0300

    Generate tweets

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+
+use App\Models\User;
+use App\Models\Tweet;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()
+            ->has(Tweet::factory()->count(3))
+            ->create();
+
+        User::factory()
+            ->has(Tweet::factory()->count(2))
+            ->create();
     }
 }
