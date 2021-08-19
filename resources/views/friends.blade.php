b29dca86282b41eef3d91f0d044d1ececac56809
dsobolev <denis.sobolev1@gmail.com>
2021-08-19 19:49:46 +0300

    Friends panel dynamic

--- resources/views/friends.blade.php
+++ resources/views/friends.blade.php
@@ -1,12 +1,12 @@
-@php
-    $friends = [1,2,3];
-    $icon = "https://i.pravatar.cc/40";
-@endphp
 <section class="bg-blue-100 rounded-lg p-4 mr-4">
     <h3 class="text-lg font-bold my-2">Friends</h2>
     <ul>
-        @foreach($friends as $friend)
-            <x-friend :name="$friend" icon="https://i.pravatar.cc/40" />
+        @foreach(auth()->user()->follows as $friend)
+            @php
+                $name = $friend->name;
+                $icon = $friend->avatar;
+            @endphp
+            <x-friend :name="$name" :icon="$icon" />
         @endforeach
     </ul>
 </section>
