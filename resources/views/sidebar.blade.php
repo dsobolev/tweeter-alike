a6e1c4acd290f6258edc2cedddee7bb6369e3178
dsobolev <denis.sobolev1@gmail.com>
2021-08-25 23:38:03 +0300

    Sidebar updated with Home, Profile

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,8 +1,12 @@
 <nav>
     <ul class="ml-8">
-        <li class="font-bold block mb-4">First</li>
+        <li class="font-bold block mb-4">
+            <a href="/dashboard">Home</a>
+        </li>
         <li class="font-bold block mb-4">Second</li>
         <li class="font-bold block mb-4">Other</li>
-        <li class="font-bold block mb-4">Something</li>
+        <li class="font-bold block mb-4">
+            <a href="{{ route('profiles.single', auth()->user()) }}">Profile</a>
+        </li>
     </ul>
 </nav>
