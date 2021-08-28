578022e31e896309740a923bf692b1d14f2c15c3
dsobolev <denis.sobolev1@gmail.com>
2021-08-28 18:37:09 +0300

    Added User::isMe method

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
@@ -101,5 +102,10 @@ class User extends Authenticatable
     public function getProfileImageAttribute()
     {
         return $this->profileImageBaseUrl . $this->username . '/750/220.webp';
+    }
+
+    public function isMe()
+    {
+        return Auth::id() === $this->id;
     }
 }
