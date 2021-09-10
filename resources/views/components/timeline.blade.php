ed27855aadd639ada3246336a47ac1e9e7a278fa
dsobolev <denis.sobolev1@gmail.com>
2021-09-10 22:09:55 +0300

    Added 'no items' signs to Friends/Tweets

--- resources/views/components/timeline.blade.php
+++ resources/views/components/timeline.blade.php
@@ -1,7 +1,9 @@
 <section class="border border-gray-400 rounded-lg my-4">
-    @foreach($tweets as $tweet)
+    @forelse($tweets as $tweet)
         <x-tweet :user="$tweet->user" :is-last="$loop->last">
             {{ $tweet->body }}
         </x-tweet>
-    @endforeach
+    @empty
+        <p class="p-4">No Tweets Yet )))</p>
+    @endforelse
 </section>
--- resources/views/friends.blade.php
+++ resources/views/friends.blade.php
@@ -1,8 +1,10 @@
 <section class="bg-blue-100 rounded-lg p-4 mr-4">
     <h3 class="text-lg font-bold my-2">Friends</h2>
     <ul>
-        @foreach($user->follows as $friend)
+        @forelse($user->follows as $friend)
             <x-friend :user="$friend" />
-        @endforeach
+        @empty
+            <span>No Friends Yet</span>
+        @endforelse
     </ul>
 </section>
